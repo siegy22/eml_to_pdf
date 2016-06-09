4915e889113fed7c17582fdf2b17b673b4d8c752
Yves Siegrist <Elektron1c97@gmail.com>
2016-06-09 15:36:38 +0200

    [ci skip] add some footer spacing in the pdf

--- lib/eml_to_pdf/wkhtmltopdf.rb
+++ lib/eml_to_pdf/wkhtmltopdf.rb
@@ -1,7 +1,7 @@
 module EmlToPdf
   class Wkhtmltopdf
     def self.convert(input, output_path)
-      IO.popen("wkhtmltopdf --encoding utf-8 --footer-center [page] - #{output_path} 2>&1", "r+") do |pipe|
+      IO.popen("wkhtmltopdf --encoding utf-8 --footer-center [page] --footer-spacing 2.5 - #{output_path} 2>&1", "r+") do |pipe|
         pipe.puts(input)
         pipe.close_write
         pipe.readlines # TODO save output
