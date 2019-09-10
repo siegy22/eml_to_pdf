2414f77d06a2145f8dac16b94cf2a2c13a419f36
Yves Siegrist <Elektron1c97@gmail.com>
2019-09-10 16:14:10 +0200

    Quieten wkhtmltopdf output
    Add --quiet flag to make the errors more readable.

--- test/wkhtmltopdf_test.rb
+++ test/wkhtmltopdf_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class WkhtmltopdfTest < MiniTest::Test
   def test_raises_if_wkhtmltopdf_fails
-    EmlToPdf::Wkhtmltopdf.convert(broken_html, TEST_FOLDER_PATH + ".." + "tmp" + "test_out.pdf")
-  rescue EmlToPdf::Wkhtmltopdf::ConversionError => e
-    assert_equal wkhtml_to_pdf_error_message, e.message
-
+    error = assert_raises EmlToPdf::Wkhtmltopdf::ConversionError do
+      EmlToPdf::Wkhtmltopdf.convert(broken_html, TEST_FOLDER_PATH + ".." + "tmp" + "test_out.pdf")
+    end
+    assert_equal("Exit with code 1 due to network error: HostNotFoundError\n", error.message)
   end
 
   def broken_html
@@ -15,6 +15,6 @@ class WkhtmltopdfTest < MiniTest::Test
   end
 
   def wkhtml_to_pdf_error_message
-    "Loading pages (1/6)\n[>                                                           ] 0%\r[======>                                                     ] 10%\r[==============================>                             ] 50%\r[============================================================] 100%\rCounting pages (2/6)                                               \n[============================================================] Object 1 of 1\rResolving links (4/6)                                                       \n[============================================================] Object 1 of 1\rLoading headers and footers (5/6)                                           \nPrinting pages (6/6)\n[>                                                           ] Preparing\r[============================================================] Page 1 of 1\rDone                                                                      \nExit with code 1 due to network error: HostNotFoundError\n"
+    "Exit with code 1 due to network error: HostNotFoundError\n"
   end
 end
