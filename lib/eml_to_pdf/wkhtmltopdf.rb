3464e32b48cc74a69e82c01b094c735646a7864c
Quentin de Metz <quentin@de.me.tz>
2020-04-07 19:15:12 +0200

    Implement ability to configure path to wkhtmltopdf

--- lib/eml_to_pdf/wkhtmltopdf.rb
+++ lib/eml_to_pdf/wkhtmltopdf.rb
@@ -3,7 +3,7 @@ module EmlToPdf
     class ConversionError < StandardError; end
 
     def self.convert(input, output_path)
-      IO.popen("wkhtmltopdf --encoding utf-8 --footer-center [page] --footer-spacing 2.5 --quiet - #{output_path} 2>&1", "r+") do |pipe|
+      IO.popen("#{EmlToPdf.configuration.wkhtmltopdf} --encoding utf-8 --footer-center [page] --footer-spacing 2.5 --quiet - #{output_path} 2>&1", "r+") do |pipe|
         pipe.puts(input)
         pipe.close_write
         output = pipe.readlines.join
